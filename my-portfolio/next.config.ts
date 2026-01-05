9bab6d985624e14d9e610db4dee9b1826ac330c4
theo-mandelbaum <mandelbaumtheo@gmail.com>
2026-01-04 21:22:02 -0500

    telling Next.js to build a static site

--- my-portfolio/next.config.ts
+++ my-portfolio/next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  output: 'export', // This tells Next.js to build a static site
 };
 
 export default nextConfig;
